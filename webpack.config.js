04627f42d3be42eb2c83799ffb05bf197e40bdbe
lucaszw <lucas.zeer@gmail.com>
2017-11-06 12:26:14 -0500

    convert to npm package

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+var config = {
+  entry: './src/three.planetransform.js',
+  output: {
+    filename: './build/bundle.js',
+    library: 'three-planetransform',
+    libraryTarget: 'commonjs2'
+  }
+};
+
+module.exports = config;
